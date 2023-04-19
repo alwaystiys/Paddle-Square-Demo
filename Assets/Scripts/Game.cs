5ecfa3a332b7295d67743affe9a865f5bfd9b9f6
alwaystiys <365825701@qq.com>
2023-04-19 20:05:22 +0800

    add test

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -10,6 +10,10 @@ public class Game : MonoBehaviour
     [SerializeField]
     Paddle bottomPaddle, topPaddle;
 
+    [SerializeField, Min(0f)]
+    Vector2 arenaExtents = new Vector2(10f, 10f);
+
+
     void Awake() => ball.StartNewGame();
 
     void Start()
@@ -20,6 +24,33 @@ public class Game : MonoBehaviour
     void Update()
     {
         ball.Move();
+        BounceYIfNeeded();
+        BounceXIfNeeded();
         ball.UpdateVisualization();
+    }
+
+    void BounceYIfNeeded()
+    {
+        float yExtents = arenaExtents.y - ball.Extents;
+        Debug.Log("ball.Position.y" + ball.Position.y);
+        if(ball.Position.y < -yExtents)
+        {
+            ball.BounceY(-yExtents);
+        }else if(ball.Position.y > yExtents)
+        {
+            ball.BounceY(yExtents);
+        }
+    }
+
+    void BounceXIfNeeded()
+    {
+        float xExtents = arenaExtents.x - ball.Extents;
+        if(ball.Position.x < -xExtents)
+        {
+            ball.BounceX(-xExtents);
+        }else if(ball.Position.x > xExtents)
+        {
+            ball.BounceX(xExtents);
+        }
     }
 }
